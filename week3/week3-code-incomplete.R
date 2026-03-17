6d3050db6980312e0a3fa0e2d4a4ca2136d39168
George Athanasopoulos <george.athanasopoulos@monash.edu>
2026-03-18 10:20:22 +1100

    Slight update week 3

--- week3/week3-code-incomplete.R
+++ week3/week3-code-incomplete.R
@@ -31,14 +31,14 @@ us_economy |>
 
 us_economy |>
   autoplot(log(GDP))
-# log may seem too strong
-# Hard to see the damping effect on variance due
+# log may seem too strong 
+# Hard to see the damping effect on variance due 
 # to the lack of noise
 
-# Note: can also take the log-transformation
-# using the Box-Cox function
-us_economy |>
-  autoplot(box_cox(GDP, 0))
+# Note: can also take the log-transformation 
+# using the Box-Cox function 
+us_economy |> 
+  autoplot(box_cox(GDP, 0)) 
 
 # Let's try something weaker
 us_economy |>
@@ -46,9 +46,9 @@ us_economy |>
 # Seems to work fairly well
 
 # Let's see what the Guerrero measure give us
-us_economy |>
+us_economy |> 
   features(GDP, features = guerrero) -> lambda
-# Attempts to balance the seasonal fluctuations
+# Attempts to balance the seasonal fluctuations 
 # and random variation across the series.
 # Always check the results.
 
@@ -56,8 +56,8 @@ lambda$lambda_guerrero
 
 us_economy |>
   autoplot(box_cox(GDP, lambda$lambda_guerrero))
-# More or less the same. Box-Cox transformations
-# are usually insensitive to the choice of lambda
+# More or less the same. Box-Cox transformations 
+# are usually insensitive to the choice of lambda 
 # at the second decimal place.
 
 ## aus_livestock ----------------------------------------------
@@ -66,8 +66,8 @@ aus_livestock |>
   autoplot(Count)
 # A bit tougher to see what is needed
 
-# Over to you
-# Try a log transformation
+# Over to you 
+# Try a log transformation 
 # What does Guerrero stat suggest?
 
 aus_livestock |>
@@ -77,19 +77,19 @@ aus_livestock |>
 
 aus_livestock |>
   filter(Animal == "Bulls, bullocks and steers", State == "Victoria") |>
-  features(Count, guerrero)
+  features(Count, guerrero) 
 # suggesting a log so we might go with that
 
 
 
-# 1.-----------------------------------------
-# Find an appropriate Box-Cox transformation in order to stabilise the variance for
+# Workshop Activity 1 -----------------------------------------
+# Find an appropriate Box-Cox transformation in order to stabilise the variance for 
 # Gas production from `aus_production`.
 
 
 ## aus_production ------------------------------------------------
 
-aus_production |>
+aus_production |> 
   autoplot(Gas)
 
 # Hint: use log
@@ -98,36 +98,36 @@ aus_production |>
 # Hint: find and use Guerrero
 
 
-# 2.-----------------------------------------
+# Workshop Activity 2 -----------------------------------------
 # Why is a Box-Cox transformation unhelpful for the `canadian_gas` data?
 
-# Monthly Canadian gas production,
+# Monthly Canadian gas production, 
 # billions of cubic metres, January 1960 - February 2005
 
 # Hint: plot the data and try a transformation
 
 
-# 3. ---------------------------------------------------------
+# Workshop Activity 3 -----------------------------------------
 us_retail_employment <- us_employment |>
   filter(year(Month) >= 1990, Title == "Retail Trade") |>
   select(-Series_ID)
-# A single series - thousands of people employed
+# A single series - thousands of people employed 
 # in Retail Trade in the US
 
 # Aim: Can we pull out the trend/cycle and seasonal component
 us_retail_employment |>
   autoplot(Employed) +
-  labs(y = "Persons (thousands)",
+  labs(y = "Persons (thousands)", 
        title = "Total employment in US retail")
 
-## STL decomposition
+## STL decomposition 
 ## US retail employment
 
 dcmp <- us_retail_employment |>
   model(stl = STL(Employed))
-# STL
+# STL 
 # Seasonal Trend decomposision
-# using LOcal regrESSions
+# using LOcal regrESSions 
 
 ## a. Plot the decomposition.---------------------------------
 
@@ -145,12 +145,12 @@ us_retail_employment |>
 
 ## d. How does the seasonal shape change over time? --------------
 
-# Hint: use gg_subseries()
+# Hint: use gg_subseries() 
 
 # Let's explore more What happens with periodic
 us_retail_employment |>
-  model(stl = STL(Employed~season(window="periodic"))) |>
-  components() |>
+  model(stl = STL(Employed~season(window="periodic"))) |> 
+  components() |> 
   autoplot()
 
 ## e. What happens as you change the values of the two `window` arguments? -------------
